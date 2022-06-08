154b62951e708812163787281c657688d894ee18
Mathues Fellipi <matheus.fellipi@outlook.com>
2022-06-08 10:53:54 -0300

    Create Calculadora.java

---
+++ src/atividad/src/Calculadora.java
@@ -0,0 +1,28 @@
+package atividad.src;
+/**
+ * calculadora
+ */
+public class Calculadora {
+
+  public static void soma(double n1, double n2) {
+    double resultados = n1 + n2;
+    System.out.println("a soma de " + n1 + " + " + n2 + " = " + resultados);
+  }
+
+  public static void subtracao(double n1, double n2) {
+    double resultados = n1 - n2;
+    System.out.println("a subtracao de " + n1 + " - " + n2 + "=" + resultados);
+  }
+
+  public static void multiplicacao(double n1, double n2) {
+    double resultados = n1 + n2;
+    System.out.println("a multiplicacao de " + n1 + " * " + n2 + "=" + resultados);
+  }
+
+  public static void divisao(double n1, double n2) {
+    double resultados = n1 + n2;
+    System.out.println("a soma de " + n1 + " / " + n2 + "=" + resultados);
+  }
+
+
+}
