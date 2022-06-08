78b773492926835c5ed2c4913a1243e6e76e6e2d
Mathues Fellipi <matheus.fellipi@outlook.com>
2022-06-08 11:49:19 -0300

    Update Quadrilatero.java

--- src/atividad/src/Quadrilatero.java
+++ src/atividad/src/Quadrilatero.java
@@ -3,22 +3,18 @@ package atividad.src;
 public class Quadrilatero {
 
   public static double area(double lado) {
-    System.out.println("Área do quadrado:" + lado * lado);
     return lado * lado;
   }
 
   public static double area(double lado1, double lado2) {
-    System.out.println("Área do retângulo:" + lado1 * lado2);
     return lado1 * lado2;
   }
 
   public static double area(double bMaior, double bMenor, double altura) {
-    System.out.println("Área do trapézio:" + ((bMaior + bMenor) * altura) / 2);
     return ((bMaior + bMenor) * altura) / 2;
   }
 
   public static float area(float diagonal1, float diagonal2) {
-    System.out.println("Área do losango:" + (diagonal1 * diagonal2) / 2);
     return (diagonal1 * diagonal2) / 2;
   }
 
